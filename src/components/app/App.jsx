906e81491b4c779361f5e4db877f0a4ac33f2015
Jonathan Quesada <jonnythan.quesada@gmail.com>
2021-03-13 03:40:46 -0800

    add sideDrawer

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,40 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, 
+  Route,
+  Switch } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import SplashPage from '../../containers/SplashPage';
+import TopDrawer from '../drawer/TopDrawer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+
+  const theme = createMuiTheme({
+    typography: {
+      fontFamily: 'Roboto Mono',
+    },
+    palette: {
+      primary: {
+        light: '#E65215',
+        main: '#d62828',
+        dark: '#6F2C38',
+      },
+      secondary: {
+        light: '#F5CD75',
+        main: '#FCBF49',
+        dark: '#FAA026',
+      }
+    }
+  });
+
+  return (
+    <Router>
+      <ThemeProvider theme={theme} >
+        <Switch>
+          <Route exact path="/" component={TopDrawer}/>
+        </Switch>
+      </ThemeProvider>
+    </Router>
+  );
 }
+
---
+++ src/containers/SplashPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './SplashPage.css';
+
+export default class SplashPage extends Component {
+  render() {
+    return (
+      <div className={styles.splashBody}>
+        <div>This is the PLACWE!</div>
+      </div>
+    );
+  }
+}
