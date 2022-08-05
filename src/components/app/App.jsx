bb46a93faf10b1eb7b4f41fca50ec8482675fe20
Jonathan Quesada <jonnythan.quesada@gmail.com>
2022-08-05 02:55:04 -0700

    remove side bar

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,37 +3,15 @@ import {
   BrowserRouter as Router, 
   Route,
   Switch } from 'react-router-dom';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import SplashPage from '../../containers/SplashPage';
-import TopDrawer from '../drawer/TopDrawer';
 
 export default function App() {
 
-  const theme = createMuiTheme({
-    typography: {
-      fontFamily: 'Roboto Mono',
-    },
-    palette: {
-      primary: {
-        light: '#E65215',
-        main: '#d62828',
-        dark: '#6F2C38',
-      },
-      secondary: {
-        light: '#F5CD75',
-        main: '#FCBF49',
-        dark: '#FAA026',
-      }
-    }
-  });
-
   return (
     <Router>
-      <ThemeProvider theme={theme} >
-        <Switch>
-          <Route exact path="/" component={TopDrawer}/>
-        </Switch>
-      </ThemeProvider>
+      <Switch>
+        <Route exact path="/" component={SplashPage}/>
+      </Switch>
     </Router>
   );
 }
--- src/containers/SplashPage.jsx
+++ src/containers/SplashPage.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import TopDrawer from '../components/drawer/TopDrawer';
 import styles from './SplashPage.css';
 
 export default class SplashPage extends Component {
   render() {
     return (
-      <div className={styles.splashBody}>
-        <div>This is the PLACWE!</div>
-      </div>
+      <main className={styles.splashBody}>
+        <TopDrawer />
+        <h1 className={styles.header}>This is the PLACE!</h1>
+        <h3 className={styles.footer}>This is a footer</h3>
+        <div className={styles.bubble}>
+        </div>
+      </main>
     );
   }
 }
